48a6e6ec75ea3cada68dc42a8d7e76b2f4cb9824
lowzhao <lowzhao@gmail.com>
2018-05-03 01:06:58 +0800

    asd

--- forest.py
+++ forest.py
@@ -5,16 +5,20 @@ from sklearn.cluster import KMeans
 from sklearn import datasets
 wine = datasets.load_wine()
 X = wine.data
+# for temp in wine.data:
+# print(wine.DESCR)
+# print(wine.data[0])
 
-Z = linkage(X,'average')
-
-plt.figure(figsize=(25, 10))
-dendrogram(Z)
-plt.show()
-
+Z = linkage(X,'complete')
+print(Z)
 kclusters = fcluster(Z, 3, criterion='maxclust')
-kclusters
+print(kclusters)
 
 km = KMeans(n_clusters=3)
 km.fit(X)
-km.labels_
+print(km.labels_)
+
+plt.figure()
+dendrogram(Z)
+plt.show()
+
