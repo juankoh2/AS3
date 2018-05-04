8d9607b629a8f7efd17517458f3ff0f960319a68
juankoh2 <kohwanjuan@yahoo.com>
2018-05-05 00:06:35 +0800

    g

--- forest.py
+++ forest.py
@@ -16,6 +16,8 @@ Y = mydata.iloc[:, 0]
 # print(wine.DESCR)
 # print(wine.data[0])
 
+
+
 Z = linkage(X,'complete')
 # print(Z)
 kclusters = fcluster(Z, 3, criterion='maxclust')
@@ -25,9 +27,19 @@ km = KMeans(n_clusters=3)
 km.fit(X)
 print(km.labels_)
 
+for i in range(0,178):
+    if kclusters[i] == 3:
+        kclusters[i] = 1
+        
+    elif kclusters[i] == 2:
+        kclusters[i] = 2
+        
+    elif kclusters[i] == 1:
+        kclusters[i] = 0
+
 
 from sklearn.metrics import accuracy_score, confusion_matrix
-print (accuracy_score(Y,kclusters))
+print (accuracy_score(km.labels_,Y))
 
 # plt.figure()
 # dendrogram(Z)
