d70545c34b65eacd9537314a5d843cce5cbf419d
juankoh2 <kohwanjuan@yahoo.com>
2018-05-04 23:19:45 +0800

    f

--- sub.py
+++ sub.py
@@ -1,23 +1,19 @@
 from matplotlib import pyplot as plt
 from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
 from sklearn.cluster import KMeans
-import pandas as pd
+
 import numpy as np
 
-from sklearn import datasets
-wine = datasets.load_wine()
-X = wine.data
-
 import pandas as pd
-mydata= pd.read_csv('full.csv')
+mydata= pd.read_csv('wine.data.csv')
 
-Y = mydata.iloc[:, 0]
+X = mydata.iloc[:, 0:6]
 # for temp in wine.data:
 # print(wine.DESCR)
 # print(wine.data[0])
 
-Z = linkage(X,'complete')
-# print(Z)
+Z = linkage(X,'average')
+print(Z)
 kclusters = fcluster(Z, 3, criterion='maxclust')
 print(kclusters)
 
@@ -25,10 +21,9 @@ km = KMeans(n_clusters=3)
 km.fit(X)
 print(km.labels_)
 
-
 from sklearn.metrics import accuracy_score, confusion_matrix
-print (accuracy_score(Y,kclusters))
+print (accuracy_score(kclusters,km.labels_))
 
-# plt.figure()
-# dendrogram(Z)
-# plt.show()
+plt.figure()
+dendrogram(Z)
+plt.show()
